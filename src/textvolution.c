afa58b90186a02b50fd81d111c87f3b1aa4ad408
nowakmr <nowakmr@umich.edu>
2019-03-29 01:56:50 -0500

    Update textvolution.c
    added logic for fitness test with ASCII-image in mind

--- src/textvolution.c
+++ src/textvolution.c
@@ -3,9 +3,10 @@
 /*
  * textvolution.cpp
  *
- * this is the top-level source file for the textvolution evolutionary text    . this file will se4t up everythin necessary top perform an actual text evolution
+ * this is the top-level source file for the textvolution evolutionary text    . 
+ * this file will set up everythin necessary top perform an actual text evolution
  *
- *
+ * Copyright 2017, 2018, 2019, Mike Nowak.
  *
  */
 
@@ -15,6 +16,57 @@
 
 int main(int argc, char* argv[]) {
 
+  
+  
+  
+  
+  
+  
+
+
+
+determine_fitness()
+
+
+
+
+
+if (compiles) {
+
+    for (int i=0; i<trials.size(); i++) {
+
+        if (no early kill needed && exits successfully) {
+            if (output exists) {
+                if (output not super long) {
+                    if (output 1 char) {
+                        if (output "y" or output "n") {
+                            if (output correct) trials[i].set_fitness(1.0);
+                            else                trials[i].set_fitness(0.8);
+                        }
+                        else trials[i].set_fitness(0.5);
+                    }
+                    else trials[i].set_fitness(0.3);
+                }
+                else trials[i].set_fitness(0.2);
+            }
+            else trials[i].set_fitness(0.1);
+        }
+        else trials[i].set_fitness(0.05);
+
+        fitness_sum += trials[i].get_fitness();
+    }
+
+    fitness = (fitness_sum / trials.size());
+}
+else {
+    fitness = 0.0;
+}
+
+
+    
+  
+  
+  
 
 
 
